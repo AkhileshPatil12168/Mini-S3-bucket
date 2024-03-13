53212e9654506762ae893ceda85547bdef299f16
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2024-03-14 02:43:30 +0530

    added authorization middleware

---
+++ src/middleware/authorization.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt");
+const {
+    isValidObjectId
+  } = require("../utils/validators");
+
+const authorization = async (req, res, next) => {
+    try {
+
+        const userId = req.params.userId;
+        const decodedToken = req.verifyed;
+    
+        if (!userId) return res.status(400).send({ status: false, message: "Please provide userId." });
+    
+        if (!isValidObjectId(userId))
+          return res.status(403).send({ status: false, message: "please login again" });
+    
+        let isCorrectUser = await bcrypt.compare(userId, decodedToken.userId);
+    
+        if (!isCorrectUser)
+          return res.status(403).send({ status: false, message: "please login again" });
+
+          next();
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message });
+    }
+};
+
+module.exports = authorization;
