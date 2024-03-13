d81147d79608e696d16c4bbcd88f38f0f80adef2
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2024-03-13 21:23:14 +0530

    get user completed with testing

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,13 +3,13 @@ const Router = express.Router();
 const authentication = require("../middleware/authentication");
 
 const createUser = require("../controllers/userControllers/createUser");
-// const getUserDetails = require("../controllers/userController/getUser");
+const getUserDetails = require("../controllers/userControllers/getUser");
 // const updateUser = require("../controllers/userController/updateUser");
 // const deleteUser = require("../controllers/userController/deleteUser");
 // const contactUs = require("../controllers/userController/contactUs");
 
 Router.post("/create/user/", createUser);
-// Router.get("/user/:userId/profile", authentication, getUserDetails);
+Router.get("/user/:userId/profile", authentication, getUserDetails);
 // Router.put("/user/:userId/profile", authentication, updateUser);
 // Router.delete("/user/:userId/profile", authentication, deleteUser);
 // Router.post("/user/:userId/contactus", authentication, contactUs);
