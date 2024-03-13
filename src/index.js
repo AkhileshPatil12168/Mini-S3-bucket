d9a62f9ba924699fe2a8e21d0c0a7b7ec1f7f06a
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2024-03-13 20:50:16 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const multer = require("multer");
+// const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const Router = require("./routes/routes")
+
+require("dotenv").config();
+
+app.use(multer().any());
+// app.use(cookieParser());
+
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:3001"],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// const Router = require("./routes/routes");
+
+app.use(express.json());
+app.use("/", Router);
+
+mongoose
+  .connect(process.env.mongoClust, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.port, () => {
+  console.log("server is live at " + process.env.PORT);
+});
