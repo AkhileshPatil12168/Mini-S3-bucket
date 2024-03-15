67efe5090d007e62a8f19ec438849d2589045f70
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2024-03-16 03:03:57 +0530

    update bucket completed with testing

---
+++ src/controllers/bucketControllers/updateBucket.js
@@ -0,0 +1,44 @@
+const bucketModel = require("../../models/bucketModel");
+
+const { isNotProvided, validTrim, isValidWord, emptyBody } = require("../../utils/validators");
+
+const updateBucket = async (req, res) => {
+  try {
+    const bucketId = req.params.bucketId;
+    let { bucketName } = req.body;
+
+    if (emptyBody(req.body))
+      return res.status(400).send({ status: false, message: "provide some data" });
+
+    const checkBucket = await bucketModel
+      .findById(bucketId)
+      .select({ _id: 1, bucketName: 1, bucketPath: 1 })
+      .lean();
+    if (!checkBucket) return res.status(404).send({ status: false, message: "bucket not found." });
+
+    if (bucketName) {
+      if (!isNotProvided(bucketName))
+        return res.status(400).send({ status: false, message: "provide the bucket name" });
+
+      bucketName = validTrim(bucketName);
+      if (!isValidWord(bucketName))
+        return res.status(400).send({ status: false, message: "enter a valid bucket name" });
+    }
+    if (checkBucket.bucketName == bucketName)
+      return res.status(404).send({ status: false, message: "use different keywords" });
+    const updatedBucket = await bucketModel
+      .findByIdAndUpdate(bucketId, { bucketName }, { new: true })
+      .select({ bucketName: 1 })
+      .lean();
+
+    return res.status(200).send({
+      status: true,
+      message: "bucket updated successful",
+      data: updatedBucket,
+    });
+  } catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+  }
+};
+
+module.exports = updateBucket;
--- src/routes/bucket.js
+++ src/routes/bucket.js
@@ -5,9 +5,11 @@ const authorization = require("../middleware/authorization");
 
 const createBucket = require("../controllers/bucketControllers/createBucket");
 const { getBuckets, getBucket } = require("../controllers/bucketControllers/getBucket");
+const updateBucket = require("../controllers/bucketControllers/updateBucket");
 
 Router.post("/user/:userId/bucket/create", authentication, authorization, createBucket);
 Router.get("/user/:userId/buckets", authentication, authorization, getBuckets);
 Router.get("/user/:userId/bucket/:bucketId", authentication, authorization, getBucket);
+Router.put("/user/:userId/bucket/:bucketId", authentication, authorization, updateBucket);
 
 module.exports = Router;
