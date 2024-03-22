78822b7adcc33b7a35a80f502ec83b3ad4036a7e
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2024-03-23 03:28:03 +0530

    api for admin home page

---
+++ src/controllers/adminControllers/get data/getServerInfo.js
@@ -0,0 +1,63 @@
+const storageModel = require("../../../models/storageModel");
+const userModel = require("../../../models/userModel");
+const connectUsModel = require("../../../models/contactUsModel");
+const bucketModel = require("../../../models/bucketModel");
+const objectModel = require("../../../models/objectModel");
+
+const getServerInformation = async (req, res) => {
+  try {
+    const data = {};
+    data.server = await storageModel.aggregate([
+      {
+        $group: {
+          _id: 0,
+          overAllBuckets: { $sum: "$totalBuckets" },
+          overAllObjects: { $sum: "$totalObjects" },
+          OverAllStorageUsed: { $sum: "$usedSpace" },
+          OverAllStorageAllocated: { $sum: "$storageSize" },
+          overAllUsers: { $sum: 1 },
+        },
+      },
+    ]);
+
+    data.storages = await storageModel
+      .find({ isDeleted: false })
+      .sort({ createdAt: -1 })
+      .select({ userId: 1, usedSpace: 1, storageSize: 1 })
+      .lean();
+
+    data.users = await userModel
+      .find({ isDeleted: false })
+      .sort({ createdAt: -1 })
+      .select({ fname: 1, lname: 1, email: 1 })
+      .limit(5)
+      .lean();
+
+    data.requests = await connectUsModel
+      .find({ resolved: false })
+      .sort({ requestedDate: -1 })
+      .select({ name: 1, email: 1, requestedDate: 1, message: 1 })
+      .limit(10)
+      .lean();
+
+    data.buckets = await bucketModel
+      .find()
+      .sort({ createdAt: -1 })
+      .select({ userId: 1, bucketName: 1 })
+      .limit(5)
+      .lean();
+
+    data.objects = await objectModel
+      .find()
+      .sort({ createdAt: -1 })
+      .select({ objectSize: 1 })
+      .limit(10)
+      .lean();
+
+    return res.status(200).send({ status: true, message: "successfull", data:{...data,server:{...data.server[0]}} });
+  } catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+  }
+};
+
+module.exports = getServerInformation;
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,9 +7,12 @@ const authorization = require("../middleware/authorization");
 const createAdmin = require("../controllers/adminControllers/createAdmin");
 const updateAdmin = require("../controllers/adminControllers/updateAdmin");
 const deleteAdmin = require("../controllers/adminControllers/deleteAdmin");
+const getServerInformation = require("../controllers/adminControllers/get data/getServerInfo");
 
 Router.post("/create/admin/", createAdmin);
 Router.put("/admin/:userId/profile", authentication, authorization, updateAdmin);
 Router.delete("/admin/:userId/profile", authentication, authorization, deleteAdmin);
+
+Router.get("/admin/:userId/serverinfo/v1", getServerInformation)
 
 module.exports = Router;
