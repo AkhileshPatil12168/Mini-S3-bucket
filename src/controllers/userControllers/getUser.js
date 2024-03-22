7f7eff1ca7e29a01d2b726b50e1f4403e3da7977
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2024-03-22 20:20:09 +0530

    removed authorization (middleware available)

--- src/controllers/userControllers/getUser.js
+++ src/controllers/userControllers/getUser.js
@@ -6,18 +6,7 @@ const { isValidObjectId } = require("../../utils/validators");
 const getUserDetails = async function (req, res) {
     try {
         const userId = req.params.userId;
-        const decodedToken = req.verifyed;
-
-        if (!userId)
-            return res.status(400).send({ status: false, message: "Please provide userId." });
-
-        if (!isValidObjectId(userId))
-        return res.status(403).send({ status: false, message: "please login again" });
-
-        let isCorrectUser = await bcrypt.compare(userId, decodedToken.userId);
-
-        if (!isCorrectUser)
-            return res.status(403).send({ status: false, message: "please login again" });
+       
 
         const userData = await userModel
             .findById(userId)
